c7f842713f6842c53f7bf951c8627e032c917b03
mithun <mithunpaul08@gmail.com>
2019-08-26 16:29:12 -0700

    ok prints final accuracy also

--- train.py
+++ train.py
@@ -206,39 +206,43 @@ class Trainer:
                 train_bar.n = 0
                 val_bar.n = 0
                 epoch_bar.update()
+
+                print(f"val loss: {running_loss} val Accuracy: {running_acc} ")
+                
+
         except KeyboardInterrupt:
             print("Exiting loop")
 
 
 
         # uncomment to compute the loss & accuracy on the test set using the best available model
+        #
+        classifier.load_state_dict(torch.load(train_state_in['model_filename']))
+        classifier = classifier.to(args_in.device)
 
-        # classifier.load_state_dict(torch.load(train_state_in['model_filename']))
-        # classifier = classifier.to(args_in.device)
-        #
-        # dataset.set_split('val')
-        # batch_generator = generate_batches(dataset,
-        #                                    batch_size=args_in.batch_size,
-        #                                    device=args_in.device)
-        # running_loss = 0.
-        # running_acc = 0.
-        # classifier.eval()
-        #
-        # for batch_index, batch_dict in enumerate(batch_generator):
-        #     # compute the output
-        #     y_pred = classifier(x_in=batch_dict['x_data'].float())
-        #
-        #     # compute the loss
-        #     loss = loss_func(y_pred, batch_dict['y_target'].float())
-        #     loss_t = loss.item()
-        #     running_loss += (loss_t - running_loss) / (batch_index + 1)
-        #
-        #     # compute the accuracy
-        #     acc_t = self.compute_accuracy(y_pred, batch_dict['y_target'])
-        #     running_acc += (acc_t - running_acc) / (batch_index + 1)
-        #
-        # train_state_in['test_loss'] = running_loss
-        # train_state_in['test_acc'] = running_acc
-        # print("Test loss: {:.3f}".format(train_state_in['test_loss']))
-        # print("Test Accuracy: {:.2f}".format(train_state_in['test_acc']))
+        dataset.set_split('val')
+        batch_generator = generate_batches(dataset,
+                                           batch_size=args_in.batch_size,
+                                           device=args_in.device)
+        running_loss = 0.
+        running_acc = 0.
+        classifier.eval()
+
+        for batch_index, batch_dict in enumerate(batch_generator):
+            # compute the output
+            y_pred = classifier(x_in=batch_dict['x_data'].float())
+
+            # compute the loss
+            loss = loss_func(y_pred, batch_dict['y_target'].float())
+            loss_t = loss.item()
+            running_loss += (loss_t - running_loss) / (batch_index + 1)
+
+            # compute the accuracy
+            acc_t = self.compute_accuracy(y_pred, batch_dict['y_target'])
+            running_acc += (acc_t - running_acc) / (batch_index + 1)
+
+        train_state_in['test_loss'] = running_loss
+        train_state_in['test_acc'] = running_acc
+        print("Val loss at end of all epochs: {:.3f}".format(train_state_in['test_loss']))
+        print("Val accuracy at end of all epochs: {:.2f}".format(train_state_in['test_acc']))
 
